392ca77d9d5aae1473f0493c37c6973eb60a846d
Ty <thopp@polity.tech>
2017-08-03 17:51:50 -0700

    created Book componenet and migrated code

---
+++ src/Book.js
@@ -0,0 +1,37 @@
+import React from 'react'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+
+class Book extends React.Component {
+
+
+ 
+	render(){
+		return(
+
+	      <li key={book.id}>
+	         <div className="book">
+	            <div className="book-top">
+	              <div className="book-cover" key={book.imageLinks.thumbnail} style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}} alt="book cover"></div>
+	              <div className="book-shelf-changer">
+	                <select onChange={(event) => this.changeShelf(book, event.target.value)} defaultValue={book.shelf}>
+	                  <option value="none" disabled>Move to...</option>
+	                  <option value="currentlyReading">Currently Reading</option>
+	                  <option value="wantToRead">Want to Read</option>
+	                  <option value="read">Read</option>
+	                  <option value="none">None</option>
+	                </select>
+	              </div>
+	            </div>
+	            <div className="book-title" key={book.title}>{book.title}</div>
+	            <div className="book-authors" key={book.authors}>{book.authors}</div>
+	          </div>
+	        
+	        </li>
+	      
+		)
+	}
+}
+
+
+export default Book
--- src/BookList.js
+++ src/BookList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Link } from 'react-router-dom'
+import Book from './Book'
 
 class BookList extends React.Component {
 
@@ -11,11 +12,6 @@ constructor(props) {
   this.changeShelf = this.changeShelf.bind(this);
 }
 
-  // empty state to be filled with API data
-	state = {
-    books: [],
-  }
-
   // represents books currently in the shelf
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
@@ -23,16 +19,7 @@ constructor(props) {
     })
   }
 
-/*
-  componentWillReceiveProps(nextProps) {
-    if(this.props.book.shelf !== nextProps.book.shelf) {
-      this.changeShelf(nextProps.book.shelf)
-    }
-  }
-*/
-
   // BooksAPI needs book object and shelf string, so need to pass it to changeShelf
-
   changeShelf = (book, shelf) => {
     BooksAPI.update(book, shelf).then(response => {
       book.shelf = shelf
@@ -41,6 +28,7 @@ constructor(props) {
       }))      
     })
   }
+
 
 	render () {
 		return (
@@ -58,30 +46,8 @@ constructor(props) {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      
-                      {this.state.books.map((book) => (
-                      <li key={book.id}>
 
-                         <div className="book">
-                            <div className="book-top">
-                              <div className="book-cover" key={book.imageLinks.thumbnail} style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}} alt="book cover"></div>
-                              <div className="book-shelf-changer">
-                                <select onChange={(event) => this.changeShelf(book, event.target.value)} defaultValue={book.shelf}>
-                                  <option value="none" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title" key={book.title}>{book.title}</div>
-                            <div className="book-authors" key={book.authors}>{book.authors}</div>
-                          </div>
-                        
-                        </li>
-                      ))}
-
+                    {this.state.books.filter(book => book.shelf === 'currentlyReading').map((book) => <Book key={book.id} book={book} />)}
 
                     </ol>
                   </div>
